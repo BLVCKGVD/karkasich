d5a81bebf06e519d0efbc2cccd00f604e34e9a42
Anton Shulzhenko <antondead1337@gmail.com>
2023-02-28 16:47:31 +0300

    bootstrap offcanvas, sidebar at seo(i think we will use it everywhere at admin panel), translations at messages.ru.yaml

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,16 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 const $ = require('jquery'); global.$ = global.jQuery = $;
-// any CSS you import will output into a single css file (app.css in this case)
+global.bootstrap = require('./js/bootstrap.bundle.min.js')
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import './styles/app.css';
 import './css/bootstrap.min.css'
 import './js/bootstrap.min.js'
+import './js/bootstrap.bundle.min.js'
+import './css/bootstrap.rtl.min.css'
+import './css/bootstrap-grid.min.css'
+import './css/bootstrap-reboot.rtl.min.css'
+import './css/bootstrap-utilities.min.css'
 
 // start the Stimulus application
 import './bootstrap';
--- src/Form/SeoType.php
+++ src/Form/SeoType.php
@@ -7,17 +7,39 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SeoType extends AbstractType
 {
+    private TranslatorInterface $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('path')
-            ->add('description')
-            ->add('robots')
-            ->add('title')
-            ->add('save', SubmitType::class)
+            ->add('path',null,[
+                'label' => $this->translator->trans('general.path')
+            ])
+            ->add('description',null,[
+                'label' => $this->translator->trans('general.description')
+            ])
+            ->add('robots',null,[
+                'label' => $this->translator->trans('general.robots')
+            ])
+            ->add('title',null,[
+                'label' => $this->translator->trans('general.title')
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => $this->translator->trans('general.save')
+            ])
         ;
     }
 
